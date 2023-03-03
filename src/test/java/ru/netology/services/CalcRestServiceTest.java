ef5fd0fa805234219dfa9614e6e7bd67e4270421
Marina Malich <e_aryukova@mail.ru>
2023-03-03 21:54:53 +0300

    Change add commit

--- src/test/java/ru/netology/services/CalcRestServiceTest.java
+++ src/test/java/ru/netology/services/CalcRestServiceTest.java
@@ -2,28 +2,36 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalcRestServiceTest {
-    @Test
-    public void  restServiceTest() {
+    @ParameterizedTest
+   // @CsvSource({
+    //        "10000,3000,20000",
+    //       "100000,60000,150000"
+   // })
+    @CsvFileSource(files="src/test/resources/rest.csv")
+    public void  testWithCsv(int income, int expenses, int threshold, int expected) {
         CalcRestService service = new CalcRestService();
-        int actual = service.calculate(10_000, 3_000, 20_000);
-        int expended = 3;
+        int actual = service.calculate(income, expenses, threshold);
+       // int expended = 3;
 
-       Assertions.assertEquals(expended, actual);
+       Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void  restServiceTest2() {
-        CalcRestService service = new CalcRestService();
-        int actual = service.calculate(100_000, 60_000, 150_000);
-        int expended = 2;
+    // @Test
+    // public void  restServiceTest2() {
+    //   CalcRestService service = new CalcRestService();
+    //   int actual = service.calculate(100_000, 60_000, 150_000);
+    //   int expended = 2;
 
-        Assertions.assertEquals(expended, actual);
+    //   Assertions.assertEquals(expended, actual);
 
-    }
+    //}
 
 
 }
